be3967c6e10ba24c5a5571173271908cc86055a0
shivamnishad0 <2k23.mca2313672@gmail.com>
2025-11-07 16:10:14 +0530

    fix for the payment checkout

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://localhost:3001/api';
-// const API_BASE_URL = 'https://mock-ecom-back.onrender.com/api';
+// const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = 'https://mock-ecom-back.onrender.com/api';
 
 export const api = {
   //  Fetch all products
